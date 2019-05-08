8b1431f68eb5e5d15c12b634ee64a10d32275424
Lars Beckmann <lars.beckmann@gmail.com>
2019-05-08 12:09:43 +0200

    Adding SwitchingGroup

---
+++ src/HomematicIp/Data/HomematicIpObjects/Groups/SwitchingGroup.cs
@@ -0,0 +1,13 @@
+﻿using HomematicIp.Data.Enums;
+
+namespace HomematicIp.Data.HomematicIpObjects.Groups
+{
+    [EnumMap(GroupType.SWITCHING)]
+
+    public class SwitchingGroup : LinkedSwitchingGroup
+    {
+        public dynamic ShutterLevel { get; set; }
+        public dynamic SlatsLevel { get; set; }
+        public bool? Processing { get; set; }
+    }
+}
