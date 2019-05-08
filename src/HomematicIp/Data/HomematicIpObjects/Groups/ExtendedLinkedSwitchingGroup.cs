e02cd3e9fde1ef6a2b7759693fe3ac54e43802e1
Lars Beckmann <lars.beckmann@gmail.com>
2019-05-08 12:44:04 +0200

    Adding ExtendedLinkedSwitchingGroup

---
+++ src/HomematicIp/Data/HomematicIpObjects/Groups/ExtendedLinkedSwitchingGroup.cs
@@ -0,0 +1,12 @@
+﻿using HomematicIp.Data.Enums;
+
+namespace HomematicIp.Data.HomematicIpObjects.Groups
+{
+    [EnumMap(GroupType.EXTENDED_LINKED_SWITCHING)]
+    public class ExtendedLinkedSwitchingGroup : LinkedSwitchingGroup
+    {
+        public double OnTime { get; set; }
+        public double OnLevel { get; set; }
+        public dynamic SensorSpecificParameters { get; set; }
+    }
+}
