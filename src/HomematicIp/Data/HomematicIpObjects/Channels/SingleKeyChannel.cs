a0b0d2d94ae4c19c11fc45428ae97a207ce22f6b
Lars Beckmann <lars.beckmann@gmail.com>
2019-05-22 14:37:16 +0200

    Fixing SingleKeyChannel

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/SingleKeyChannel.cs
@@ -0,0 +1,5 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(Enums.FunctionalChannelType.SINGLE_KEY_CHANNEL)]
+    public class SingleKeyChannel : DeviceSabotageChannelBase { }
+}
