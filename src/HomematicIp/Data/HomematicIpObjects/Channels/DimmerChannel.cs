4fbda270e3984f665c085d19bc00a0088baaa5a0
Lars Beckmann <lars.beckmann@gmail.com>
2019-05-22 15:48:02 +0200

    Adding DimmerChannel and PluggableDimmer

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/DimmerChannel.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(Enums.FunctionalChannelType.DIMMER_CHANNEL)]
+    public class DimmerChannel : DeviceSabotageChannelBase
+    {
+        public string ProfileMode { get; set; }
+        public string UserDesiredProfileMode { get; set; }
+        public double? DimLevel { get; set; }
+        [JsonProperty(PropertyName = "on")]
+        public bool? IsOn { get; set; }
+    }
+}
---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/PluggableDimmer.cs
@@ -0,0 +1,5 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(Enums.DeviceType.PLUGGABLE_DIMMER)]
+    public class PluggableDimmer : Device { }
+}
