954921001d044795d0e42d077e44ed327f8f690d
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-05-23 11:11:47 +0200

    Make WindowState nullable for ShutterContact
    ```csharp
    Newtonsoft.Json.JsonSerializationException: Cannot convert null value to HomematicIp.Data.Enums.WindowState. Path 'windowState', line 13, position 21.
    ```

--- src/HomematicIp/Data/HomematicIpObjects/Channels/ShutterContactChannel.cs
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/ShutterContactChannel.cs
@@ -5,7 +5,7 @@ namespace HomematicIp.Data.HomematicIpObjects.Channels
     [EnumMap(FunctionalChannelType.SHUTTER_CONTACT_CHANNEL)]
     public class ShutterContactChannel : DeviceSabotageChannelBase
     {
-        public WindowState WindowState { get; set; }
+        public WindowState? WindowState { get; set; }
         public int EventDelay { get; set; }
     }
 }
