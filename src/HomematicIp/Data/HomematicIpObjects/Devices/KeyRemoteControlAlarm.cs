2c182a566417a82c1ba892926705018d2f854530
Lars Beckmann <lars.beckmann@gmail.com>
2019-05-24 16:33:21 +0200

    Adding KeyRemoteControlAlarm

---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/KeyRemoteControlAlarm.cs
@@ -0,0 +1,8 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(Enums.DeviceType.KEY_REMOTE_CONTROL_ALARM)]
+    public class KeyRemoteControlAlarm : Device
+    {
+
+    }
+}
