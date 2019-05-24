9fa060bbe01b61457090f88b468174f77c03fd47
Lars Beckmann <lars.beckmann@gmail.com>
2019-05-24 13:41:06 +0200

    Adding support for PermanentFullRx

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/DevicePermanentFullRx.cs
@@ -0,0 +1,8 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(Enums.FunctionalChannelType.DEVICE_PERMANENT_FULL_RX)]
+    public class DevicePermanentFullRx : AbstractDeviceBase
+    {
+        public bool? PermanentFullRx { get; set; }
+    }
+}
