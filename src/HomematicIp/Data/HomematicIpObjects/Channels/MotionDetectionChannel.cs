e6f4c40136ac8ffcef2d05ac6d50ebb214484b57
Lars Beckmann <lars.beckmann@gmail.com>
2019-05-24 09:49:33 +0200

    Adding support for MotionDetectorPushButton

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/MotionDetectionChannel.cs
@@ -0,0 +1,15 @@
+﻿using HomematicIp.Data.Enums;
+
+namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(Enums.FunctionalChannelType.MOTION_DETECTION_CHANNEL)]
+    public class MotionDetectionChannel : DeviceSabotageChannelBase
+    {
+        public bool? MotionDetected { get; set; }
+        public dynamic Illumination { get; set; }
+        public dynamic CurrentIllumination { get; set; }
+        public double? NumberOfBrightnessMeasurements { get; set; }
+        public MotionDetectionSendInterval MotionDetectionSendInterval { get; set; }
+        public bool? MotionBufferActive { get; set; }
+    }
+}
---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/MotionDetectorPushButton.cs
@@ -0,0 +1,5 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(Enums.DeviceType.MOTION_DETECTOR_PUSH_BUTTON)]
+    public class MotionDetectorPushButton : Device { }
+}
