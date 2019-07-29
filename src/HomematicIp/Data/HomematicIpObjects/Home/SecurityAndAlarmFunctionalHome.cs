ebf42d8bf354eaa13d0f8152196b7a49b8d31c36
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-07-29 16:39:21 +0200

    some properties can be null

--- src/HomematicIp/Data/HomematicIpObjects/Home/SecurityAndAlarmFunctionalHome.cs
+++ src/HomematicIp/Data/HomematicIpObjects/Home/SecurityAndAlarmFunctionalHome.cs
@@ -14,24 +14,24 @@ namespace HomematicIp.Data.HomematicIpObjects.Home
         public string AlarmEventDeviceId { get; set; }
         public string AlarmEventTriggerId { get; set; }
         public AlarmEventDeviceChannel AlarmEventDeviceChannel { get; set; }
-        public SecurityJournalEntryType AlarmSecurityJournalEntryType { get; set; }
+        public SecurityJournalEntryType? AlarmSecurityJournalEntryType { get; set; }
         public bool AlarmActive { get; set; }
         [JsonConverter(typeof(TimespanConverter))]
         public TimeSpan? SafetyAlarmEventTimestamp { get; set; }
         public SafetyAlarmEventDeviceChannel SafetyAlarmEventDeviceChannel { get; set; }
-        public SecurityJournalEntryType SafetyAlarmSecurityJournalEntryType { get; set; }
+        public SecurityJournalEntryType? SafetyAlarmSecurityJournalEntryType { get; set; }
         public bool SafetyAlarmActive { get; set; }
         [JsonConverter(typeof(TimespanConverter))]
         public TimeSpan? IntrusionAlarmEventTimestamp { get; set; }
         public string IntrusionAlarmEventTriggerId { get; set; }
         public IntrusionAlarmEventDeviceChannel IntrusionAlarmEventDeviceChannel { get; set; }
-        public SecurityJournalEntryType IntrusionAlarmSecurityJournalEntryType { get; set; }
+        public SecurityJournalEntryType? IntrusionAlarmSecurityJournalEntryType { get; set; }
         public bool IntrusionAlarmActive { get; set; }
         public SecurityZones SecurityZones { get; set; }
         public SecuritySwitchingGroups SecuritySwitchingGroups { get; set; }
         public double ZoneActivationDelay { get; set; }
         public bool IntrusionAlertThroughSmokeDetectors { get; set; }
-        public SecurityZoneActivationMode SecurityZoneActivationMode { get; set; }
+        public SecurityZoneActivationMode? SecurityZoneActivationMode { get; set; }
         public bool ActivationInProgress { get; set; }
     }
 }
