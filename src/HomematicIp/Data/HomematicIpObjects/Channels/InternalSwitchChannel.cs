274284edc0590377e0024b674e16fa6505bf3f00
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-09-09 10:59:20 +0200

    device BRAND_WALL_MOUNTED_THERMOSTAT added

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/InternalSwitchChannel.cs
@@ -0,0 +1,12 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(Enums.FunctionalChannelType.INTERNAL_SWITCH_CHANNEL)]
+    public class InternalSwitchChannel : DeviceSabotageChannelBase
+    {
+        public bool InternalSwitchOutputEnabled { get; set; }
+        public string HeatingValveType { get; set; }
+        public double ValveProtectionDuration { get; set; }
+        public double ValveProtectionSwitchingInterval { get; set; }
+        public double FrostProtectionTemperature { get; set; }
+    }
+}
--- src/HomematicIp/Data/HomematicIpObjects/Channels/WallMountedThermostatProChannel.cs
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/WallMountedThermostatProChannel.cs
@@ -8,6 +8,7 @@ namespace HomematicIp.Data.HomematicIpObjects.Channels
         public double? ActualTemperature { get; set; }
         public double? Humidity { get; set; }
         public double? VaporAmount { get; set; }
+        public double? SetPointTemperature { get; set; }
         public ClimateControlDisplay Display { get; set; }
     }
 }
