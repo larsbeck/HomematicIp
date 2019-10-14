016a9f60aa7de80bf23baad7f2006c3b7e4402db
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-10-14 14:01:24 +0200

    added FULL_FLUSH_CONTACT_INTERFACE

---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/FullFlushContactInterface.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using HomematicIp.Data.Enums;
+using HomematicIp.Data.HomematicIpObjects.Channels;
+
+namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(DeviceType.FULL_FLUSH_CONTACT_INTERFACE)]
+    public class FullFlushContactInterface : SabotageDevice
+    {
+        private ShutterContactChannel ShutterContactChannel =>
+            FunctionalChannels?.OfType<ShutterContactChannel>().FirstOrDefault();
+
+        public WindowState? WindowState => ShutterContactChannel?.WindowState;
+        public MultiModeInputMode? MultiModeInputMode { get; set; }
+        public BinaryBehaviorType? BinaryBehaviorType { get; set; }
+    }
+}
