22c37c748fd3efd6f0c05bda8f56db00a33d090b
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-10-14 13:03:11 +0200

    added missing group type SwitchingProfile

---
+++ src/HomematicIp/Data/HomematicIpObjects/Groups/SwitchingProfile.cs
@@ -0,0 +1,15 @@
+﻿using HomematicIp.Data.Enums;
+using Newtonsoft.Json;
+
+namespace HomematicIp.Data.HomematicIpObjects.Groups
+{
+    [EnumMap(GroupType.SWITCHING_PROFILE)]
+    public class SwitchingProfile : Group
+    {
+        public double? DimLevel { get; set; }
+        public string ProfileMode { get; set; }
+        public string ProfileId { get; set; }
+        [JsonProperty(PropertyName = "on")]
+        public bool? IsOn { get; set; }
+    }
+}
