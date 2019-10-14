55da0a8bb12ca0434dc3000da4f74bee8af2a8ea
Lars Beckmann <lars.beckmann@gmail.com>
2019-10-14 16:40:36 +0200

    Adding support for FLOOR_TERMINAL_BLOCK_12

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/HeatDemandChannel.cs
@@ -0,0 +1,7 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(Enums.FunctionalChannelType.HEAT_DEMAND_CHANNEL)]
+    public class HeatDemandChannel : DeviceSabotageChannelBase
+    {
+    }
+}
