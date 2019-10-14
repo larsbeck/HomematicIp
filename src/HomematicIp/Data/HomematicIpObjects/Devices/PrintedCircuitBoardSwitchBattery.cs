03713ce1b0c0724f599d7c56fbf03520ce016d06
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-10-14 14:09:20 +0200

    added PrintedCircuitBoardSwitchBattery (HmIP-PCBS-BAT)

---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/PrintedCircuitBoardSwitchBattery.cs
@@ -0,0 +1,8 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(Enums.DeviceType.PRINTED_CIRCUIT_BOARD_SWITCH_BATTERY)]
+    public class PrintedCircuitBoardSwitchBattery : Device
+    {
+
+    }
+}
