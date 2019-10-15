8c16e10b1a1204f7bb1ec5b96fc6145f13f6498f
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-10-15 13:02:48 +0200

    added support for HmIP-FCI6

---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/FullFlushContactInterface6.cs
@@ -0,0 +1,9 @@
+﻿using HomematicIp.Data.Enums;
+
+namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(DeviceType.FULL_FLUSH_CONTACT_INTERFACE_6)]
+    public class FullFlushContactInterface6 : SabotageDevice
+    {
+    }
+}
