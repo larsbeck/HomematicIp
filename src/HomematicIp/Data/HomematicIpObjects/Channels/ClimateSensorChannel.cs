b3b4b087efab55845c24828295d7b8495a5c8726
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-10-15 12:07:12 +0200

    added ClimateSensorChannel channel and the device TemperatureHumiditySensorOutdoor

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/ClimateSensorChannel.cs
@@ -0,0 +1,10 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(Enums.FunctionalChannelType.CLIMATE_SENSOR_CHANNEL)]
+    public class ClimateSensorChannel : ThermostatChannelBase
+    {
+        public double? ActualTemperature { get; set; }
+        public double? Humidity { get; set; }
+        public double? VaporAmount { get; set; }
+    }
+}
---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/TemperatureHumiditySensorOutdoor.cs
@@ -0,0 +1,7 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(Enums.DeviceType.TEMPERATURE_HUMIDITY_SENSOR_OUTDOOR)]
+    public class TemperatureHumiditySensorOutdoor : Device
+    {
+    }
+}
