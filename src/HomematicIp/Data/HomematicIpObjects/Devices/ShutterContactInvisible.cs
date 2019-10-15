c985640c355f649094e80a6ad93ec60201ce3970
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-10-15 12:40:05 +0200

    added support for HmIP-SWDO-I

---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/ShutterContactInvisible.cs
@@ -0,0 +1,7 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(Enums.DeviceType.SHUTTER_CONTACT_INVISIBLE)]
+    public class ShutterContactInvisible : SabotageDevice
+    {
+    }
+}
