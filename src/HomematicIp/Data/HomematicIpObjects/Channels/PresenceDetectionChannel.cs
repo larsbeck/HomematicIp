e9af7c9ff178481ebe05848c2ab535cb66488398
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-10-15 12:14:04 +0200

    added PresenceDetectionChannel

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/PresenceDetectionChannel.cs
@@ -0,0 +1,15 @@
+﻿using HomematicIp.Data.Enums;
+
+namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(FunctionalChannelType.MOTION_DETECTION_CHANNEL)]
+    public class PresenceDetectionChannel : DeviceSabotageChannelBase
+    {
+        public bool? PresenceDetected { get; set; }
+        public double? Illumination { get; set; }
+        public double? CurrentIllumination { get; set; }
+        public double? NumberOfBrightnessMeasurements { get; set; }
+        public MotionDetectionSendInterval MotionDetectionSendInterval { get; set; }
+        public bool? MotionBufferActive { get; set; }
+    }
+}
