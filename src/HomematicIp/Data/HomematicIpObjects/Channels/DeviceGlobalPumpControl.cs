d455c28b118e3b14f3435148688483de7008ea35
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-10-16 15:07:32 +0200

    added channel DEVICE_GLOBAL_PUMP_CONTROL

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/DeviceGlobalPumpControl.cs
@@ -0,0 +1,23 @@
+﻿using HomematicIp.Data.Enums;
+using Newtonsoft.Json;
+
+namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(FunctionalChannelType.DEVICE_GLOBAL_PUMP_CONTROL)]
+    public class DeviceGlobalPumpControl : AbstractDeviceBaseChannel
+    {
+        public double? ValveProtectionDuration { get; set; }
+        public double? ValveProtectionSwitchingInterval { get; set; }
+        public double? FrostProtectionTemperature { get; set; }
+        public double? CoolingEmergencyValue { get; set; }
+        public double? HeatingEmergencyValue { get; set; }
+        public double? MinimumFloorHeatingValvePosition { get; set; }
+
+        [JsonProperty(PropertyName = "pulseWidthModulationAtLowFloorHeatingValvePositionEnabled")]
+        public bool IsPulseWidthModulationAtLowFloorHeatingValvePositionEnabled { get; set; }
+        [JsonProperty(PropertyName = "globalPumpControl")]
+        public bool IsGlobalPumpControl { get; set; }
+        public HeatingValveType HeatingValveType { get; set; }
+        public HeatingValveType HeatingLoadType { get; set; }
+    }
+}
