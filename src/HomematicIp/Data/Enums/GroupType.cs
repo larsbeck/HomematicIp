0291584b1df9382eee7246a3168bd39a50a2bb07
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-10-18 13:23:51 +0200

    added HEAT_DEMAND_RULE

--- src/HomematicIp/Data/Enums/GroupType.cs
+++ src/HomematicIp/Data/Enums/GroupType.cs
@@ -35,6 +35,7 @@ namespace HomematicIp.Data.Enums
         ENVIRONMENT,
         SECURITY_BACKUP_ALARM_SWITCHING,
         META,
-        SHUTTER_PROFILE
+        SHUTTER_PROFILE,
+        HEAT_DEMAND_RULE
     }
 }
