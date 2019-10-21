101b4a94c463c3f217e03cf0a576fb0767917d82
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2019-10-21 15:02:33 +0200

    added support for HmIP-MOD-HO

---
+++ src/HomematicIp/Data/Enums/DoorState.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace HomematicIp.Data.Enums
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum DoorState
+    {
+        OPEN,
+        CLOSED,
+        TILTED
+    }
+}
---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/DoorChannel.cs
@@ -0,0 +1,18 @@
+﻿using HomematicIp.Data.Enums;
+using Newtonsoft.Json;
+
+namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(FunctionalChannelType.DOOR_CHANNEL)]
+    public class DoorChannel : DeviceSabotageChannelBase
+    {
+        public DoorState? DoorState { get; set; }
+        [JsonProperty(PropertyName = "processing")]
+        public bool? IsProcessing { get; set; }
+        [JsonProperty(PropertyName = "on")]
+        public bool? IsOn { get; set; }
+        [JsonProperty(PropertyName = "ventilationPositionSupported")]
+        public bool? IsVentilationPositionSupported { get; set; }
+
+    }
+}
