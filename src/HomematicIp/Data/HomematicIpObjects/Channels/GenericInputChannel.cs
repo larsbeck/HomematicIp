714ebb8b21175d4b7bb33d3217d37bdea9880abb
Eduard Schäfer <eschaefer@easy-smarthome.de>
2019-11-12 15:42:55 +0100

    added GENERIC_INPUT_CHANNEL

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/GenericInputChannel.cs
@@ -0,0 +1,9 @@
+﻿using HomematicIp.Data.Enums;
+
+namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(FunctionalChannelType.GENERIC_INPUT_CHANNEL)]
+    public class GenericInputChannel : SabotageDevice
+    {
+    }
+}
