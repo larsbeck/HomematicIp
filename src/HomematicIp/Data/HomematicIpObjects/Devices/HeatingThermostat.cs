4da7ce1d259a8d632c6d7805ff8cbebbb26ee3a0
Lars Beckmann <lars.beckmann@gmail.com>
2020-03-19 16:12:08 +0100

    Adding HEATING_THERMOSTAT_COMPACT to EnumMap

--- src/HomematicIp/Data/HomematicIpObjects/Devices/HeatingThermostat.cs
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/HeatingThermostat.cs
@@ -1,6 +1,6 @@
 ﻿namespace HomematicIp.Data.HomematicIpObjects.Devices
 {
-    [EnumMap(Enums.DeviceType.HEATING_THERMOSTAT)]
+    [EnumMap(Enums.DeviceType.HEATING_THERMOSTAT, Enums.DeviceType.HEATING_THERMOSTAT_COMPACT)]
     public class HeatingThermostat:Device
     {
         public bool? AutomaticValveAdaptionNeeded { get; set; }
