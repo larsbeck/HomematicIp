a8538b39fb1aeb4b7b26353eda1cf325894dde9c
Eduard Schäfer <eschaefer@easy-smarthome.de>
2020-04-01 11:38:47 +0200

    added device HmIP-DRSI4 (DIN_RAIL_SWITCH_4) and channel MULTI_MODE_INPUT_SWITCH_CHANNEL

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/MultiModeInputSwitchChannel.cs
@@ -0,0 +1,16 @@
+﻿using HomematicIp.Data.Enums;
+using Newtonsoft.Json;
+
+namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(FunctionalChannelType.MULTI_MODE_INPUT_SWITCH_CHANNEL)]
+    public class MultiModeInputSwitchChannel : Channel
+    {
+        [JsonProperty(PropertyName = "on")]
+        public bool? IsOn { get; set; }
+        public string ProfileMode { get; set; }
+        public string UserDesiredProfileMode { get; set; }
+        public MultiModeInputMode? MultiModeInputMode { get; set; }
+        public BinaryBehaviorType? BinaryBehaviorType { get; set; }
+    }
+}
