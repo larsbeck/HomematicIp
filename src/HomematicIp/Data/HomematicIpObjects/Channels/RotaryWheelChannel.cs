d98c44f5574ada83b182012574b62c5d0f38de4e
Eduard Schäfer <eschaefer@easy-smarthome.de>
2020-05-08 16:46:21 +0200

    added HmIP-WRCR

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/RotaryWheelChannel.cs
@@ -0,0 +1,10 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(Enums.FunctionalChannelType.ROTARY_WHEEL_CHANNEL)]
+    public class RotaryWheelChannel : Channel
+    {
+        //"rotationDirection": "CLOCK_WISE" we could use enum in the future..
+        public string RotationDirection { get; set; }
+        public double? EventDelay { get; set; }
+    }
+}
