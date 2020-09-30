6ec629a78b6b97c17a5a8db7569bee00f4d678aa
Eduard Schäfer <eschaefer@easy-smarthome.de>
2020-09-30 17:25:03 +0200

    added windowOpen prop to MULTI_MODE_INPUT_CHANNEL

--- src/HomematicIp/Data/HomematicIpObjects/Channels/MultiModeInputChannel.cs
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/MultiModeInputChannel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using HomematicIp.Data.Enums;
 using HomematicIp.Data.HomematicIpObjects.Channels;
+using Newtonsoft.Json;
 
 namespace HomematicIp.Data.HomematicIpObjects.Devices
 {
@@ -13,5 +14,9 @@ namespace HomematicIp.Data.HomematicIpObjects.Devices
         public WindowState? WindowState => ShutterContactChannel?.WindowState;
         public MultiModeInputMode? MultiModeInputMode { get; set; }
         public BinaryBehaviorType? BinaryBehaviorType { get; set; }
+
+        //https://github.com/iobroker-community-adapters/ioBroker.hmip/blob/be548cf299c54bef0cc0fcc94231a949a7e9736c/main.js#L1423
+        [JsonProperty(PropertyName = "windowOpen")]
+        public bool? IsWindowOpened { get; set; }
     }
 }
