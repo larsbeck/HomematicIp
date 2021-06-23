f2c2292ab873a0189353fcd4a82b199f524899e2
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2021-06-23 14:52:50 +0200

    support IMPULSE_OUTPUT_CHANNEL & WALL_MOUNTED_GARAGE_DOOR_CONTROLLER

---
+++ src/HomematicIp/Data/HomematicIpObjects/Channels/ImpulseOutputChannel.cs
@@ -0,0 +1,13 @@
+﻿using HomematicIp.Data.Enums;
+using Newtonsoft.Json;
+
+namespace HomematicIp.Data.HomematicIpObjects.Channels
+{
+    [EnumMap(FunctionalChannelType.IMPULSE_OUTPUT_CHANNEL)]
+    public class ImpulseOutputChannel : Channel
+    {
+        [JsonProperty(PropertyName = "processing")]
+        public bool? IsProcessing { get; set; }
+        public double? ImpulseDuration { get; set; }
+    }
+}
