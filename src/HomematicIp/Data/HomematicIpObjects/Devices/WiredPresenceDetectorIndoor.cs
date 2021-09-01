b7f9129e59315ea8290fea9d99f158893ec99df1
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2021-09-01 14:16:53 +0200

    added support for HmIPW-SPI

---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/WiredPresenceDetectorIndoor.cs
@@ -0,0 +1,8 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(Enums.DeviceType.WIRED_PRESENCE_DETECTOR_INDOOR)]
+    public class WiredPresenceDetectorIndoor : Device
+    {
+
+    }
+}
