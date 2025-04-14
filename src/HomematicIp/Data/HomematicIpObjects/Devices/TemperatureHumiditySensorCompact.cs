823a933ba11cefdec11932046060f6c4c16359a5
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2025-04-14 10:07:43 +0200

    add support for ELV-SH-CTH, ELV-SH-CTV and ELV-SH-SB8

---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/TemperatureHumiditySensorCompact.cs
@@ -0,0 +1,7 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(Enums.DeviceType.TEMPERATURE_HUMIDITY_SENSOR_COMPACT)]
+    public class TemperatureHumiditySensorCompact : Device
+    {
+    }
+}
---
+++ src/HomematicIp/Data/HomematicIpObjects/Devices/TiltVibrationSensorCompact.cs
@@ -0,0 +1,7 @@
+﻿namespace HomematicIp.Data.HomematicIpObjects.Devices
+{
+    [EnumMap(Enums.DeviceType.TILT_VIBRATION_SENSOR_COMPACT)]
+    public class TiltVibrationSensorCompact : Device
+    {
+    }
+}
